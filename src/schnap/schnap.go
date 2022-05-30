cb70f88914f60b73126118b43b3ab865819586f1
Dennis Mahle <dennis.mahle@joyent.com>
2022-05-29 22:19:51 -0700

    add editHandler

--- src/schnap/schnap.go
+++ src/schnap/schnap.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 )
 
@@ -24,9 +25,35 @@ func loadPage(title string) (*Page, error) {
 	return &Page{Title: title, Body: body}, nil
 }
 
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/view/"):]
+	p, _ := loadPage(title)
+	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit/"):]
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+		fmt.Fprintf(w, "<h1>Editing %s</h>"+
+			"<form action=\"/save/%s\" method=\"POST\">"+
+			"<textarea name=\"body\">%s</textarea><br>"+
+			"<input type=\"submit\" value=\"Save\">"+
+			"</form>", p.Title, p.Title, p.Body)
+	}
+}
+
 func main() {
+	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
+	http.HandleFunc("/save/", saveHandler)
+	http.ListenAndServe(":8080", nil)
+
+	/* Initial test code
 	p1 := &Page{Title: "TestPage", Body: []byte("This is a sample page.")}
 	p1.save()
 	p2, _ := loadPage("TestPage")
 	fmt.Println(string(p2.Body))
+	*/
 }
