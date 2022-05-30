6621af4861deb675bf1960128bfc7c23c772afd5
Dennis Mahle <dennis.mahle@joyent.com>
2022-05-29 22:44:37 -0700

    add templating

--- src/schnap/schnap.go
+++ src/schnap/schnap.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"net/http"
 	"os"
 )
@@ -25,10 +25,15 @@ func loadPage(title string) (*Page, error) {
 	return &Page{Title: title, Body: body}, nil
 }
 
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	t, _ := template.ParseFiles(tmpl + ".html")
+	t.Execute(w, p)
+}
+
 func viewHandler(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/view/"):]
 	p, _ := loadPage(title)
-	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+	renderTemplate(w, "view", p)
 }
 
 func editHandler(w http.ResponseWriter, r *http.Request) {
@@ -36,18 +41,14 @@ func editHandler(w http.ResponseWriter, r *http.Request) {
 	p, err := loadPage(title)
 	if err != nil {
 		p = &Page{Title: title}
-		fmt.Fprintf(w, "<h1>Editing %s</h>"+
-			"<form action=\"/save/%s\" method=\"POST\">"+
-			"<textarea name=\"body\">%s</textarea><br>"+
-			"<input type=\"submit\" value=\"Save\">"+
-			"</form>", p.Title, p.Title, p.Body)
 	}
+	renderTemplate(w, "edit", p)
 }
 
 func main() {
 	http.HandleFunc("/view/", viewHandler)
 	http.HandleFunc("/edit/", editHandler)
-	http.HandleFunc("/save/", saveHandler)
+	// http.HandleFunc("/save/", saveHandler)
 	http.ListenAndServe(":8080", nil)
 
 	/* Initial test code
